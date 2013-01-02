22b9cb708ff9bff3e57d548bde71ea7c36f5d61a
Alex Kotomanov <alex@kotomanov.com>
2013-01-02 04:17:37 +0400

    Optimization: move array initialization to viewDidLoad method

--- CrystalBall/ViewController.m
+++ CrystalBall/ViewController.m
@@ -17,7 +17,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    predictionArray = [[NSArray alloc] initWithObjects:@"It is certain",
+                                @"It is decidedly so",
+                                @"All signs say YES",
+                                @"The stars are not aligned",
+                                @"My reply is no",
+                                @"It is doubtful",
+                                @"Better not tell you now",
+                                @"Concentrate and ask again",
+                                @"Unable to answer now", nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,16 +36,6 @@
 }
 
 - (IBAction)buttonPressed:(UIButton *)sender {
-    NSArray *predictionArray = [[NSArray alloc] initWithObjects:@"It is certain",
-                       @"It is decidedly so",
-                       @"All signs say YES",
-                       @"The stars are not aligned",
-                       @"My reply is no",
-                       @"It is doubtful",
-                       @"Better not tell you now",
-                       @"Concentrate and ask again",
-                       @"Unable to answer now", nil];
-
     self.predictionLabel.text = [predictionArray objectAtIndex:5];
 }
 @end
